a503c3eba2d844eeef04bf27086d67b0780f7d52
Aniket Marwade <65933448+aniketmarwade@users.noreply.github.com>
2021-11-22 14:55:52 +0530

    query to create table for sql like clause practice

---
+++ Source_Like.sql
@@ -0,0 +1,19 @@
+
+--Table creation query
+create table student(
+stud_id int,
+f_name varchar(25),
+l_name varchar(25),
+phone int,
+email varchar(50),
+city varchar(20),
+pincode int);
+
+--Insert Query 
+insert into student values(1,'Aniket','Marwade',987654,'ani@gmail.com','Nagpur',441601),
+(2,'Pranav','Patil',89898,'pra@hotmail.com','Yavatmal',443123),
+(3,'Anand','Babar',790908,'ana@gmail.com','Nashik',503012),
+(4,'Sidharth','Patil',68765,'sid@gmail.com','Solapur',431644),
+(5,'Vaishnavi','Salunke',78765,'vai@gmail.com','Mumbai',566160),
+(6,'Aditi','Patil',98765,'adi@gmail.com','Osmanabad',445001),
+(7,'Harshit','Vijay',68766,'har@gmail.com','Jaipur',224451);
